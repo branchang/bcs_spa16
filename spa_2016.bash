4256c3eb6b0a02500a73e1f77f6de1ceafdd0f5b
Nick Rozanski <nick@rozanski.org.uk>
2016-06-11 09:59:23 +0100

    added yarn-site and hadoop-client

--- spa_2016.bash
+++ spa_2016.bash
@@ -2,16 +2,17 @@
 # start or stop Big data services
 
 # set environment
+echo
 source "$(dirname $0)"/env.src
 echo The project directory is $SPA_2016.
 echo
 
 start() {
-    echo ''
     echo '===> Starting Hadoop...'
     $HADOOP_PREFIX/sbin/hadoop-daemon.sh start namenode
     $HADOOP_PREFIX/sbin/hadoop-daemon.sh start secondarynamenode 
     $HADOOP_PREFIX/sbin/hadoop-daemon.sh start datanode
+    $HADOOP_PREFIX/sbin/start-yarn.sh
     echo ''
     echo '===> Starting Spark...'
     $SPARK_HOME/sbin/start-master.sh
@@ -24,7 +25,6 @@ start() {
 }
 
 stop() {
-    echo ''
     echo '===> Stopping Hive...'
     pid="$(ps -e | grep HiveServer2 | grep -v 'grep ' | awk '{print $1}')"
     if [ -n "$pid" ]; then
@@ -40,6 +40,7 @@ stop() {
     $SPARK_HOME/sbin/stop-all.sh
     echo ''
     echo '===> Stopping Hadoop...'
+    $HADOOP_PREFIX/sbin/stop-yarn.sh
     $HADOOP_PREFIX/sbin/stop-dfs.sh
 }
 
@@ -48,17 +49,27 @@ status() {
     jps="$(which jps 2>/dev/null)"
     [ -z "$jps" ] && jps=$JAVA_HOME/bin/jps
     $jps | grep -v Jps | sort -k 2
+    echo
     echo 'Hive Process:'
     ps -e | grep HiveServer | grep -v 'grep '
     echo
 }
 
+hadoop_client() {
+    client_url='http://localhost:50070/explorer.html#/'
+    [ "$(uname)" == Linux ] && sensible-browser $client_url && return
+    [ "$(uname)" == Darwin ] && open $client_url && return
+    echo 'Not yet supported for Cygwin'
+}
+
 [ "$1" == start ] && start && exit
 [ "$1" == stop ] && stop && exit
 [ "$1" == status ] && status && exit
+
+[ "$1" == hadoop-client ] && hadoop_client && exit
 [ "$1" == spark-client ] && $SPARK_HOME/bin/pyspark && exit
 [ "$1" == hive-client ] && $SPARK_HOME/bin/beeline -u jdbc:hive2:// --color && exit
 
-echo "Syntax: $0 [ start | stop | status | spark-client | hive-client ]"
+echo "Syntax: $0 [ start | stop | status | hadoop-client | spark-client | hive-client ]"
 exit 1
 
